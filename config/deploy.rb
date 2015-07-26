b23352415def76eb85daa1f8de0bc548cdf09981
Harris Novick <harrisdavidnovick@gmail.com>
2015-07-25 20:04:24 -0400

    add secrets to linked_files

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :ssh_options, {
 set :use_sudo, true
 set :pty, false
 
-set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 set :linked_dirs,  fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 SSHKit.config.command_map[:rake]  = "bundle exec rake"
