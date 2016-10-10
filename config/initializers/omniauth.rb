bc5d3e35e425693e2222d7911e825d3b395a5ec9
Harris Novick <harrisdavidnovick@gmail.com>
2016-10-09 20:03:21 -0400

    Remove Rails

--- config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, Rails.application.secrets.omniauth_provider_key, Rails.application.secrets.omniauth_provider_secret
-end
--- spec/factories/users.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :user do
-    name "MyString"
-provider "MyString"
-uid "MyString"
-  end
-
-end
