498b78dc743c5e65edc287af82f1bc9181c4782d
Harris Novick <harrisdavidnovick@gmail.com>
2016-12-28 23:33:50 -0500

    Implement hero-ui post-type-3 in jade/scss

--- routes/index.js
+++ routes/index.js
@@ -13,14 +13,16 @@ var twitter = new Twitter({
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  twitter.get('statuses/user_timeline', { screen_name: 'theMTA', count: 1 },  function(error, tweets, response) {
-    if (!error) {
-      res.status(200).render('index', { title: 'Harris Novick', tweet: tweets[0].text });
-    }
-    else {
-      res.status(500).json({ error: error });
-    }
-  });
+  res.status(200).render('index', { quotation: 'Aquaman is a superhero appearing in comic book titles by DC Comics. Created by Paul Norris and Mort Weisinger' });
+  // twitter.get('statuses/user_timeline', { screen_name: 'theMTA', count: 1 },  function(error, tweets, response) {
+  //   if (!error) {
+  //     console.log(tweets[0]);
+  //     res.status(200).render('index', { title: 'Harris Novick', tweet: tweets[0].text });
+  //   }
+  //   else {
+  //     res.status(500).json({ error: error });
+  //   }
+  // });
 });
 
 module.exports = router;
